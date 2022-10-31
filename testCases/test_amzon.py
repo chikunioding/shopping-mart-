dd59a067eb43d52b0bec66bc4035f80b24cdf4be
chikunioding <115853668+chikunioding@users.noreply.github.com>
2022-10-31 21:06:34 +0530

    Create test_amzon.py

---
+++ testCases/test_amzon.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+driver=webdriver.Chrome(r"C:\Users\Mayur\PycharmProjects\shopping mart\driver\chromedriver.exe")
+driver.get(r"https://www.amazon.in/")
