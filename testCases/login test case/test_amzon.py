3dd2c8f86c71a10d70d3d832e80b2ae92d3dc25b
mayur <mayur.nioding.mn@gmail.com>
2022-10-31 21:47:29 +0530

    this is 2nd commit

--- testCases/login test case/test_amzon.py
+++ testCases/login test case/test_amzon.py
@@ -4,3 +4,9 @@ import time
 
 driver=webdriver.Chrome(r"C:\Users\Mayur\PycharmProjects\shopping mart\driver\chromedriver.exe")
 driver.get(r"https://www.amazon.in/")
+
+time.sleep(2)
+
+print(driver.title)
+
+driver.quit()
